64e969fbb6b7b20ce15561ec07dfa6bef0163341
jacek <mendyk.ja.ubuntu@gmail.com>
2021-07-25 18:18:09 +0200

    upgrading dog.py

--- dog.py
+++ dog.py
@@ -1,4 +1,4 @@
-class Dog():
+class Dog:
     """Simple class for representing model of dog"""
 
     def __init__(self, name, age):
@@ -13,3 +13,9 @@ class Dog():
     def roll_over(self):
         """Simulation that dog lays on his back after taking a command"""
         print(f"{self.name.title()} is  lying on his back now")
+
+
+my_dog = Dog('willie', 6)
+
+print(f"My dog name is {my_dog.name.title()}")
+print(f"My dog is {my_dog.age} years old")
