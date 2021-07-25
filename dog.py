2251db59f5b407b6babd556c796d020e9fa0dd8b
jacek <mendyk.ja.ubuntu@gmail.com>
2021-07-25 18:04:03 +0200

    simulation of dog

--- dog.py
+++ dog.py
@@ -1,2 +1,15 @@
 class Dog():
-    pass
+    """Simple class for representing model of dog"""
+
+    def __init__(self, name, age):
+        """Initialization attributes name and age"""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Simulation that dog sits after taking a command"""
+        print(f"{self.name.title()} is sitting now")
+
+    def roll_over(self):
+        """Simulation that dog lays on his back after taking a command"""
+        print(f"{self.name.title()} is  lying on his back now")
