1b5be442f1ce9bb18a7a93c45626554bbbbc15fa
jacek <mendyk.ja@gmail.com>
2021-08-09 18:32:14 +0200

    creating module car based on file 'car.py'

--- car.py
+++ car.py
@@ -1,3 +1,6 @@
+"""Class used for representing a car"""
+
+
 class Car:
     """Simple car representation trial"""
 
@@ -31,12 +34,3 @@ class Car:
         """Incrementation of odometer's value by a given value"""
         self.odometer_reading += miles
 
-
-my_used_car = Car('subaru', 'outback', 2015)
-print(my_used_car.get_descriptive_name())
-
-my_used_car.update_odometer(23_500)
-my_used_car.read_odometer()
-
-my_used_car.increment_odometer(100)
-my_used_car.read_odometer()
