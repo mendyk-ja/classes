83b5c2ecc5ebc0ec2800d527072fadf38643ddf6
jacek <mendyk.ja@gmail.com>
2021-08-13 18:02:41 +0200

    the last commit here

--- lottery.py
+++ lottery.py
@@ -5,7 +5,7 @@ my_list = ['a', 'b', 'c ', 'd', 'e', '1', '2', '3', '4', '5', '6', '7', '8', '9'
 print(f"Lottery wins a person, who has that number or letter: {choice(my_list)}")
 
 """This piece of code is checking how many times you would needed to draw to win the lottery."""
-lottery_number = None 
+lottery_number = None
 my_number = choice(my_list)
 my_ticket = []
 number_of_draw = 0
@@ -13,4 +13,3 @@ while lottery_number != my_number:
     lottery_number = choice(my_list)
     number_of_draw += 1
 print(f"You would won after {number_of_draw} times draw.")
-
